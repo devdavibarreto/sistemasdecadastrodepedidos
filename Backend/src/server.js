c1841db39052de23ff72ec3d41af7558ffe0fea3
DaviBarretoo <davibarretojkda@gmail.com>
2024-09-02 10:20:15 -0300

    fixed error backend cors

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const  atos  = require("./atos.routes")
 const app = express();
-
+const cors = require ("cors")
 app.use(express.json());
+app.use(cors)
 app.use(atos)
 
 app.get('/teste',(req,res)=>{
--- Frontend/atos/src/App.js
+++ Frontend/atos/src/App.js
@@ -1,11 +1,16 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import {AiOutlineEdit,AiOutlineDelete} from "react-icons/ai";
+import axios from "axios"
 
 const arraypedidos =[{pedidos: "burgão", descricao: "burgão00000000000", ordem: 1},
   {pedidos: "burgão", descricao: "burgão00000000000", ordem: 2}
 ]
 
 const AtosPrincipal = ({atosBurguer})=>{
+
+
+
   return(
 <div className="atosPrincipal">
 {atosBurguer.map(atos =>{
@@ -30,10 +35,24 @@ const AtosPrincipal = ({atosBurguer})=>{
 };
 
 export default function App() {
+async function getPedidos() {
+  const response = await axios.get("https://localhost:3523")
+  console.log(response)
+}
+
+const [pedidos,setPedidos] = useState([])
+useEffect(()=>{
+  getPedidos();
+},[])
+
   return (
     <div className="App">
       <header className="container">
-
+<div className='header'>
+  <input className='inputName'></input>
+  <input className='inputName'></input>
+  <button className='newPedidoRegister'>Registrar</button>
+</div>
        <AtosPrincipal atosBurguer={arraypedidos}></AtosPrincipal>
      
       </header>
